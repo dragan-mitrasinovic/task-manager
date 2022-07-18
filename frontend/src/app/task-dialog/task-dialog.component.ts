940f8b6200a5838a2ad85b7252a62ae7f0c3e8e2
dragan <dragan.mitrasinovic19@gmail.com>
2022-07-18 22:31:19 +0200

    Added a basic dialog component

---
+++ frontend/src/app/task-dialog/task-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-task-dialog',
+  templateUrl: './task-dialog.component.html',
+  styleUrls: ['./task-dialog.component.css'],
+})
+export class TaskDialogComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string) {}
+
+  ngOnInit(): void {}
+}
