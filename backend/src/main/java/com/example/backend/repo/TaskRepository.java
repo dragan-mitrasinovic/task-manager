431658caec32479aa0222ef389cd1af73c699efe
dragan <dragan.mitrasinovic19@gmail.com>
2022-07-21 22:17:29 +0200

    Sorting projects by date

--- backend/src/main/java/com/example/backend/repo/TaskRepository.java
+++ backend/src/main/java/com/example/backend/repo/TaskRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, UUID> {
 
-  List<TaskEntity> findAllByColumnId(int columnId);
+  List<TaskEntity> findAllByColumnIdAndProjectId(int columnId, UUID projectId);
 }
